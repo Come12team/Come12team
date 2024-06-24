47e0bca4bb96534ba45ea5b2cebb6bc41f5e7457
chl1195 <160118949+chl1195@users.noreply.github.com>
2024-06-25 03:06:31 +0900

    [Feature] SceneMover.cs Add, Test
    씬 이동 스크립트 추가, 테스트

--- Assets/Script/UI/ButtonAction.cs
+++ Assets/Script/UI/ButtonAction.cs
@@ -45,16 +45,4 @@ public class ButtonAction : MonoBehaviour
     {
         panel.SetActive(false);
     }
-
-    // 게임 종료
-    public void QuitGame()
-    {
-        // 구동 환경이 유니티 에디터일 경우
-#if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
-        // 구동 환경이 응용프로그램일 경우
-        #else
-            Application.Quit();
-        #endif
-    }
 }
