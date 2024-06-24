0c1ddd183a607908b9781f9585034a71e92e0e5a
dhrzld <dhrzld@gmail.com>
2024-06-24 10:31:08 +0900

    조금 추가한거
    조금 추가한거

--- Assets/Script/Monster/BossMonster.cs
+++ Assets/Script/Monster/BossMonster.cs
@@ -8,13 +8,15 @@ public class BossMonster : MonoBehaviour
     private Transform[] wayPoints;
     private int currentIndex = 0;
     private MonsterManager monsterManager;
-
+    //private Player player; // 플레이어(소환수)?
+    //private System.Random random = new System.Random();
     public int health = 100;
     public float timeLimit = 30f; // 30초제한
 
     private void Start()
     {
         StartCoroutine(StartTimer()); // 타이머
+        //StartCoroutine(IncreaseHealthPeriodically());
     }
 
     public void Setup(Transform[] wayPoints)
@@ -59,11 +61,21 @@ public class BossMonster : MonoBehaviour
 
     public void TakeDamage(int damage)
     {
+        //if (random.NextDouble() <= 0.03) //3퍼센트확률로 공격무효
+        //{
+        //    Debug.Log("Attack blocked!");
+        //    return;
+        //}
         health -= damage;
         if (health <= 0)
         {
             Die();
         }
+        //if (random.NextDouble() <= 0.05)
+        //{
+        //    Debug.Log("소환사 플레이어 공격속도?");
+        //    player.공격속도?(0.4f, 3.0f); // 40% 느려지게 공격속도가
+        //}
     }
 
     private void Die()
@@ -80,6 +92,30 @@ public class BossMonster : MonoBehaviour
             //GameOver(); // 게임 오버 처리
         }
     }
+
+    //private void StunPlayer(float duration) //플레이어?스턴적용하게
+    //{
+    //    if (player != null)
+    //    {
+    //        player.Stun(duration);
+    //    }
+    //}
+
+    
+    //private void AttackPlayer()
+    //{
+    //    //1초간 스턴
+    //    StunPlayer(1.0f);
+    //}
+
+    //private IEnumerator IncreaseHealthPeriodically() //보스몬스터가 10초마다 체력20회복
+    //{
+    //    while (true)
+    //    {
+    //        yield return new WaitForSeconds(10f);
+    //        health += 20;
+    //    }
+    //}
 
     //private void GameOver()
     //{
